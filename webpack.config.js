61a1bfbd30fbb4c47e18792194f6a52153e389ef
yanyi_mac <yanyi9399@outlook.com>
2018-03-26 15:16:18 +0800

    已经引入公共头部尾部

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const headerHtml = require('./src/assets/js/header');
 module.exports = {
-	//entry: ['./src/a.js','./src/b.js'] //通用，多入口到单一出口
-	entry: {
-		a: './src/a.js',
-		b: './src/b.js'  
+	entry: ['./src/assets/js/a.js','./src/assets/js/b.js'],//通用，多入口到单一出口
+	// entry: {
+	// 	a: './src/assets/js/a.js',
+	// 	b: './src/assets/js/b.js'  
 
-	},
+	// },
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name]-bundle.js'
+		filename: 'bundle-[hash].js'
 	},
 	plugins:[
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'templete A',
 			filename: 'a.html',
-			template: './src/templetes/tpla.html'
+			template: './src/a.html',
+			chunks: ['b'],
+			files:{
+				css: ['main.css','a.css'],
+				header: headerHtml
+			},
 		}),
 		new HtmlWebpackPlugin({
 			title: 'templete B',
 			filename: 'b.html',
-			template: './src/templetes/tplb.html'
+			template: './src/b.html',
 		}),
 	]
 }
