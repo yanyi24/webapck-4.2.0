755a4d67fb71207dec5efd5e3c577d9e249af355
yanyi_mac <yanyi9399@outlook.com>
2018-03-26 11:46:19 +0800

    B

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
 	//entry: ['./src/a.js','./src/b.js'] //通用，多入口到单一出口
 	entry: {
@@ -10,4 +12,17 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name]-bundle.js'
 	},
+	plugins:[
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'templete A',
+			filename: 'a.html',
+			template: './src/templetes/tpla.html'
+		}),
+		new HtmlWebpackPlugin({
+			title: 'templete B',
+			filename: 'b.html',
+			template: './src/templetes/tplb.html'
+		}),
+	]
 }
